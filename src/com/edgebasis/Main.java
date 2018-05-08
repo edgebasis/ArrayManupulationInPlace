09057180d2fdc59e582b57a8f970dbbd073f35ea
Mujtaba Mahmood <mujtabamahmood@Mujtabas-MacBook-Pro.local>
2018-05-07 21:01:22 -0400

    better time complexity upgrade

--- src/com/edgebasis/Main.java
+++ src/com/edgebasis/Main.java
@@ -2,25 +2,31 @@ package com.edgebasis;
 
 public class Main {
 
-    static int[] arr = new int[] {0,2,3,0,4,0,6};
+    static int[] arr = new int[] {0,0,0,2,0,3,0,4,0,6};
 
     public static void main(String[] args) {
 	// write your code here
-        int tempVal = 0;
-
+        int tempVal;
+        int lastNonZero=0;
         for(int i =0; i< arr.length; i++){
             int counter=i;
-
+            lastNonZero = i;
             if(arr[i]==0)
             {
 
 
-                while(arr[counter]==0 && (counter)<arr.length-1) {
-                    counter++;
+
+                for(int j =lastNonZero+1; j<arr.length;j++)
+                {
+                    if(arr[j]!=0){
+                        tempVal=arr[j];
+                        arr[j]=arr[i];
+                        arr[i]=tempVal;
+                        lastNonZero=j;
+                    }
                 }
-                tempVal=arr[counter];
-                arr[counter]=arr[i];
-                arr[i]=tempVal;
+
+
              }
 
         }
